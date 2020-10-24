22fb59a1ad135d8aed3c79df4a651e90badebca0
dmacmakes <dmacmakes@gmail.com>
2020-10-24 15:27:01 +1100

    improvement on first project given out - sdl_image libraries properly placed and tested, image load via sdl to opengl surface added

---
+++ imgui_load_image/dmgui.cpp
@@ -0,0 +1,79 @@
+#include "dmgui.h"
+#include <SDL_image.h>
+#include <string>
+#include "CImg.h"
+using cimg_library::cimg::nearest_pow2;
+
+namespace DmGui
+{
+  std::string Load_Image_To_GLuint_Texture(std::string& path, DmGui::ImageTexture& image_texture) // int& width, int& height)
+  {
+    SDL_Surface* image = nullptr;
+    //image = IMG_Load("./snes_pad_start_scene.png");
+    //SDL_RWops* rwop;
+    //rwop = SDL_RWFromFile("snes_pad_start_scene.png", "rb");
+    //image = IMG_LoadPNG_RW(rwop);
+    //image = IMG_Load_RW(rwop, 0);
+    image = IMG_Load(path.c_str());
+    std::string image_error = "none";
+    if (!image) {
+      image_error = IMG_GetError();
+    }
+    //int texture_width = image->w;
+    //int texture_height = image->h;
+    image_texture.width = image->w;
+    image_texture.height = image->h;
+    image_error = "width: " + std::to_string(image_texture.width) + "height: " + std::to_string(image_texture.height);
+
+
+    // http://forums.libsdl.org/viewtopic.php?t=8166 //////// SDL Surface TO GL Texture
+    /* OpenGL pixel format for destination surface. */
+    int bpp;
+    Uint32 Rmask, Gmask, Bmask, Amask;
+    SDL_PixelFormatEnumToMasks(SDL_PIXELFORMAT_ABGR8888, &bpp, &Rmask,
+      &Gmask, &Bmask, &Amask);
+
+    /* Create surface that will hold pixels passed into OpenGL. */
+    SDL_Surface* img_rgba8888 = SDL_CreateRGBSurface(0, image_texture.width, image_texture.height, bpp,
+      Rmask, Gmask, Bmask, Amask);
+
+    /*
+     * Disable blending for source surface. If this is not done, all
+     * destination surface pixels end up with crazy alpha values.
+     */
+    SDL_SetSurfaceAlphaMod(image, 0xFF);
+    SDL_SetSurfaceBlendMode(image, SDL_BLENDMODE_NONE);
+
+    /* Blit to this surface, effectively converting the format. */
+    SDL_BlitSurface(image, NULL, img_rgba8888, NULL);
+
+    /* Store width and height as return values. */
+    //assert(w && h);
+    //*w = image->w;
+    //*h = image->h;
+    //unsigned pow_w = nearest_pow2(image->w);
+    //unsigned pow_h = nearest_pow2(image->h);
+    // Adding bits from
+    // https://github.com/ocornut/imgui/wiki/Image-Loading-and-Displaying-Examples#Example-for-OpenGL-users
+    /*
+     * Create a blank texture with power-of-two dimensions. Then load
+     * converted image data into its lower left.
+     */
+    //GLuint img_texture;
+    
+    glGenTextures(1, &(image_texture.texture_id));
+    glBindTexture(GL_TEXTURE_2D, image_texture.texture_id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_texture.width, image_texture.width, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    // Use this if you prefer to go to nearest power of two. Good for performance, bad for image display.
+    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, pow_w, pow_h, 0,GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, image_texture.width, image_texture.height, GL_RGBA,
+      GL_UNSIGNED_BYTE, img_rgba8888->pixels);
+
+    //glBindTexture(GL_TEXTURE_2D, img_texture);
+    SDL_FreeSurface(img_rgba8888);
+    return image_error;
+  }
+}
---
+++ imgui_load_image/dmgui.h
@@ -0,0 +1,29 @@
+///////////////////////////////////////////////////////////////
+//  -------------
+//  dmgui.h
+//  -------------
+//  Helper functions and bits for the CAO107 imgui + SDL2
+//  setup for weeks 5-8.
+//
+///////////////////////////////////////////////////////////////
+
+#pragma once
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <string>
+
+
+
+namespace DmGui 
+{
+  struct ImageTexture
+  {
+    GLuint texture_id{0};
+    int width{0};
+    int height{0};
+    std::string error;
+    bool isVisible {true};
+  };
+
+  std::string Load_Image_To_GLuint_Texture(std::string& path, DmGui::ImageTexture& image_texture);
+}
